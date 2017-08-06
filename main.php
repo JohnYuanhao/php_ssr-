853bb9e810be406a8e32c7f15f2889e5e1a67ebf
Shadower <934405375@qq.com>
2017-08-06 14:28:19 +0800

    php网络爬虫

---
+++ main.php
@@ -0,0 +1,92 @@
+﻿<?php
+
+error_reporting(E_ALL & ~E_NOTICE);
+//错误信息等级降级
+//$url = "http://localhost/iShadow.htm";
+$url = "http://ss.ishadowx.com/";
+//$url=$_GET["url"];
+
+//编码
+function urlsafe_b64encode($string) {
+	$data = base64_encode($string);
+	$data = str_replace(array('+', '/'), array('-', '_'), $data);
+	return rtrim($data, "=");
+}
+
+//解码
+function urlsafe_b64decode($string) {
+	$data = str_replace(array('-', '_'), array('+', '/'), $string);
+	$mod4 = strlen($data) % 4;
+	if ($mod4) {
+		$data .= substr('====', $mod4);
+	}
+	return base64_decode($data);
+	//return $data;
+}
+
+function array2object($array) {
+	if (is_array($array)) {
+		$obj = new StdClass();
+		foreach ($array as $key => $val) {
+			$obj -> $key = $val;
+		}
+	} else { $obj = $array;
+	}
+	return $obj;
+}
+
+function object2array($object) {
+	if (is_object($object)) {
+		foreach ($object as $key => $value) {
+			$array[$key] = $value;
+		}
+	} else {
+		$array = $object;
+	}
+	return $array;
+}
+
+//include("main.php");
+require_once "php_thief.php";
+
+//加密aes-256-cfb  协议origin 混淆plain
+class shadows {
+	var $server;
+	//服务器地址
+	var $server_port;
+	//服务器端口
+	var $password;
+	//密码
+	var $protocol;
+	//协议
+	var $method;
+	//加密
+	var $obfs;
+	//混淆
+	var $remarks;
+	//备注
+	var $group;
+	//组名
+}
+
+// print_r($arr);
+array_unshift($arr,array('server'=>"127.0.0.1",'server_port'=>"1080",'password' => "0" ,'method' => "none",'remarks' =>"本组服务器来源-ss.ishadowx.com")); 
+// $arr[] = array('server'=>"127.0.0.1",'server_port'=>"1080",'password' => "0" ,'method' => "none",'remarks' =>"本组服务器来源-ss.ishadowx.com");
+
+$myfile = fopen("ishadow.txt", "w") or die("Unable to open file!");
+fwrite($myfile,"MAX=99\n");
+foreach ($arr as $i => $val) {
+	$string = $arr[$i][server] . ":" . $arr[$i][server_port] . ":origin:" . $arr[$i][method] . ":plain:" . urlsafe_b64encode($arr[$i][password]);
+	$string = $string . "/?obfsparam=&remarks=" . urlsafe_b64encode($arr[$i][remarks]) . "&group=" . urlsafe_b64encode("@Shadow_影:1");
+	fwrite($myfile, "ssr://" . urlsafe_b64encode($string) . "\n");
+}
+fclose($myfile);
+
+$file_path = "ishadow.txt";
+if(file_exists($file_path)){
+$fp = fopen($file_path,"r");
+$str = fread($fp,filesize($file_path));//指定读取大小，这里把整个文件内容读取出来
+echo $str;
+// echo urlsafe_b64encode("@Shadow_影:2");
+}
+?>
---
+++ php_thief.php
@@ -0,0 +1,118 @@
+﻿<?php
+
+error_reporting(E_ALL & ~E_NOTICE);//错误信息等级降级
+
+function fetch_urlpage_contents($url) {
+	$c = file_get_contents($url);
+	return $c;
+}
+
+//获取匹配内容
+function fetch_match_contents($begin, $end, $c) {
+	$begin = change_match_string($begin);
+	$end = change_match_string($end);
+	$p = "#{$begin}(.*){$end}#iU";
+	//i表示忽略大小写，U禁止贪婪匹配
+	if (preg_match_all($p, $c, $rs)) {
+		return $rs;
+	} else {
+		return "";
+	}
+}//转义正则表达式字符串
+
+function change_match_string($str) {
+	//注意，以下只是简单转义
+	$old = array("/", "$", '?');
+	$new = array("/", "$", '?');
+	$str = str_replace($old, $new, $str);
+	return $str;
+}
+
+//采集网页
+function pick($url, $ft, $th) {
+	$c = fetch_urlpage_contents($url);
+	foreach ($ft as $key => $value) {
+		$rs[$key] = fetch_match_contents($value["begin"], $value["end"], $c);
+		if (is_array($th[$key])) {
+			foreach ($th[$key] as $old => $new) {
+				$rs[$key] = str_replace($old, $new, $rs[$key]);
+			}
+		}
+	}
+	return $rs;
+}
+
+//删除指定内容
+function delByValue($arr, $value){
+    if(!is_array($arr)){
+        return $arr;
+    }
+	$k=array_search("$value",$arr);//搜索数组中指定的元素并返回位置
+	while ($k>-1) {
+		array_splice($arr,$k,1);//移除数组指定位置的元素
+		$k=array_search("$value",$arr);
+	}
+	//删除查看图片链接
+//	foreach($arr as $k=>$v){
+//		if(($k+1)%5==0){
+//			unset($arr[$k]);
+//		}
+//	}
+	$arr=array_merge($arr);//重新排序
+    return $arr;
+}
+
+//改数组为键值对象
+function changeArray($arr){
+	$arr=array_chunk($arr,5,FALSE);
+	foreach ($arr as $key => $val) {
+		//$arr[$key][0]=str_replace("IP Address:", "", $arr[$key][0]);
+		$arr[$key][0]=str_replace(['">','</span>'],"",fetch_match_contents('">','</span>',$arr[$key][0])[0][0]);
+		$arr[$key][1]=str_replace("Port：", "", $arr[$key][1]);
+		//$arr[$key][2]=str_replace("Password:", "", $arr[$key][2]);
+		$arr[$key][2]=str_replace(['">','</span>'],"",fetch_match_contents('">','</span>',$arr[$key][2])[0][0]);
+		if(trim($arr[$key][2])==null){
+			unset($arr[$key]);
+			continue;
+		}
+		$arr[$key][3]=str_replace("Method:", "", $arr[$key][3]);
+		$arr[$key][4]=str_replace('"','',str_replace('title="', "",fetch_match_contents('title="','"',$arr[$key][4])[0][0]));
+		$a=array("server","server_port","password","method","remarks");
+		$c=array_combine($a,$arr[$key]);
+		$arr[$key]=$c;
+	}
+	$arr=array_merge($arr);
+	return $arr;
+}
+
+function delByIp($arr){
+	$num=[];
+	foreach($arr as $k => $v){
+		$flag=true;
+		foreach ($num as $key => $value) {
+			if($v[server]==$value[server]){
+				$flag=false;
+			}
+		}
+		if($flag){
+			$num[]=$v;
+			// $flag=true;
+		}
+	}
+	return $num;
+}
+
+//要采集的地址
+$ft["a"]["begin"] = '<h4>';
+//截取的开始点<br />
+$ft["a"]["end"] = '</h4>';
+//截取的结束点
+
+$rs = pick($url, $ft, $th);
+//开始采集
+$arr=$rs["a"][1];
+$arr=delByValue($arr,'auth_sha1_v4 tls1.2_ticket_auth');//去除无用代码
+$arr=changeArray($arr);//转换数组为二维
+$arr=delByIp($arr);
+//print_r($arr);
+?>
